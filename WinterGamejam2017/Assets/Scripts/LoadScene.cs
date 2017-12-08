24535925869799d2c59d3b84a9850b4078c6a195
Alisa Latic <alisa.latic5@gmail.com>
2017-12-08 21:52:15 +0100

    start

---
+++ WinterGamejam2017/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+
+    public void OnMouseClick(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
