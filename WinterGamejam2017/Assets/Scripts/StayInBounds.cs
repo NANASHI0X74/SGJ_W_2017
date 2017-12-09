b6d4665e3d2b3496915df1f25ba43b90d1c9fa1b
Alisa Latic <alisa.latic5@gmail.com>
2017-12-09 19:11:32 +0100

    spawner

---
+++ WinterGamejam2017/Assets/Scripts/StayInBounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInBounds : MonoBehaviour {
+
+    public AIFans m_fanObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider _other)
+    {
+        Debug.Log(_other.name);
+        if (_other.GetComponent<GameObject>() == null)
+        {
+
+        }
+        else if (_other.GetComponent<GameObject>().tag == ("Wall"))
+        {
+            Transform fanTransform = m_fanObject.GetComponent<Transform>();
+            Quaternion fanRotation = fanTransform.rotation;
+            float yRotation = fanTransform.rotation.y;
+            yRotation += 90;
+            fanTransform.rotation = new Quaternion (fanTransform.rotation.x, yRotation, fanTransform.rotation.z, fanTransform.rotation.w);
+            //m_fanObject.m_fanMovement = new Vector3(m_fanObject.m_fanMovement.x, yRotation, m_fanObject.m_fanMovement.z);
+            Debug.Log("entered");
+        }
+    }
+}
