3264c4620122cb34401034a684b55b7734df7930
Alisa Latic <alisa.latic5@gmail.com>
2017-12-10 10:08:28 +0100

    UI Scene

--- WinterGamejam2017/Assets/Scripts/CooldownBar.cs
+++ WinterGamejam2017/Assets/Scripts/CooldownBar.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BarScript : MonoBehaviour
+public class CooldownBar : MonoBehaviour
 {
 
     public Image bar;
-    public float m_coolDown = 15.0f;
-    public float counter;
+    public float m_coolDown = 30.0f;
+    public float counter = 0;
 
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+    public bool coolingDown;
 
     // Update is called once per frame
     void Update()
     {
-
-        if (counter <= m_coolDown:)
-            bar.fillAmount = counter++ * Time.deltaTime / m_coolDown;
-        else {
+        counter += Time.deltaTime;
+        if (counter <= 1.0f && coolingDown)
+        {
+            bar.fillAmount = counter;
+        }
+        else
+        {
+            counter = 0;
+            coolingDown = false;
 
         }
     }
--- WinterGamejam2017/Assets/Scripts/PlayerControler.cs
+++ WinterGamejam2017/Assets/Scripts/PlayerControler.cs
@@ -17,7 +17,7 @@ public class PlayerControler : MonoBehaviour {
         public float upwardThrowStrength = 10.0f;
         public float horizontalThrowStrength = 10.0f;
         public float kickback = 2.0f;
-        public SpriteRenderer Clothes;
+        public GameObject Clothes;
         private int floorMask;
         public float camRayLength = 500.0f;
         public float maxMoveSpeed = 100.0f;
@@ -54,7 +54,7 @@ public class PlayerControler : MonoBehaviour {
         {
                 if (Input.GetButtonDown("Fire1") && clothesCounter > 0){
                         clothes--;
-                        SpriteRenderer fired = Instantiate(Clothes, transform.position + transform.forward, transform.rotation);
+                        GameObject fired = Instantiate(Clothes, transform.position + transform.forward, transform.rotation);
                         fired.GetComponent<Rigidbody>().velocity = transform.forward * horizontalThrowStrength+ Vector3.up * upwardThrowStrength;
                         fired.GetComponent<ClothesBehavior>().setDisplayedItem(5 - clothes);
                         m_latestClothes = fired;
